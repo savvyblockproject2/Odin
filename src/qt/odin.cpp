2a897751307d753d81a76c6051ac744670376833
tohsnoom <moonshot@phore.io>
2018-09-30 14:18:14 -0500

    Reversed order of Q_INIT_RESOURCE lines.

--- src/qt/odin.cpp
+++ src/qt/odin.cpp
@@ -530,8 +530,8 @@ int main(int argc, char* argv[])
     QTextCodec::setCodecForCStrings(QTextCodec::codecForTr());
 #endif
 
-    Q_INIT_RESOURCE(odin_locale);
     Q_INIT_RESOURCE(odin);
+    Q_INIT_RESOURCE(odin_locale);
 
     BitcoinApplication app(argc, argv);
 #if QT_VERSION > 0x050100
